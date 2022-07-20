c7bba14f7b83d06a5a0077be6f69ca6199eb8051
voronrk <voron_76@mail.ru>
2022-07-20 05:12:57 +0300

    Try to add ElasticSearch (not completed)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,17 @@
 {{ $data }}
+{{-- <?php
+$url = 'https://hawking.kb.us-central1.gcp.cloud.es.io:9243';
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_SSL_VERIFYPEER => 0,
+    CURLOPT_POST => 1,
+    CURLOPT_HEADER => 0,
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => $url,
+));
+$result = json_decode(curl_exec($curl),1);
+echo "<pre>";
+echo print_r($result,true);
+echo "</pre>";
+curl_close($curl); 
+?> --}}
